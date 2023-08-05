33154af35beddbab4fc15b101f8b729977b27e52
DkG-07 <dkg.0717@gmail.com>
2023-08-05 09:59:16 -0600

    Avances en characters

--- app/characters/page.tsx
+++ app/characters/page.tsx
@@ -1,13 +1,23 @@
 'use client';
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
+import styles from './characters.module.css'
+
+interface RMC {
+  name: string;
+  image: string;
+  species: string;
+}
 
 const Characters = () => {
+
+  const [characters, setCharacters] = useState([])
 
   const getCharacters = async () => {
     try {
       const response = await fetch('https://rickandmortyapi.com/api/character/?page=1')
-      const data = await response.json()
-      console.log(data)
+      const { results } = await response.json()
+      console.log(results)
+      setCharacters(results)
     } catch {
 
     }
@@ -18,7 +28,21 @@ const Characters = () => {
   }, [])
 
   return (
-    <div>Characters</div>
+    <section className={styles.characters_container}>
+      <h1 className={styles.rick_characters}>Characters</h1>
+      {characters.length > 0 && <div className={styles.characters_list}>
+        {characters.map((character: RMC, idx) => {
+          return (
+            <div className={styles.character_item} key={idx} style={{ backgroundImage: `url(${character?.image})` }}>
+              <div className={styles.character_info}>
+                <p>{character.name}</p>
+                <p>{character.species}</p>
+              </div>
+            </div>
+          )
+        })}
+      </div>}
+    </section>
   )
 }
 
