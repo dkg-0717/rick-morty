e472a64bd72d20db9b98f585bcac6cdc26a68d38
DkG-07 <dkg.0717@gmail.com>
2023-08-05 10:43:24 -0600

    Inicio de paginador

--- app/characters/page.tsx
+++ app/characters/page.tsx
@@ -10,13 +10,16 @@ interface RMC {
 
 const Characters = () => {
 
+  const [totalPages, setTotalPages] = useState(0)
   const [characters, setCharacters] = useState([])
   const [charactersCopy, setCharactersCopy] = useState([])
 
   const getCharacters = async () => {
     try {
       const response = await fetch('https://rickandmortyapi.com/api/character/?page=1')
-      const { results } = await response.json()
+      const data = await response.json()
+      console.log(data)
+      const { results } = data
       setCharactersCopy(results)
       setCharacters(results)
     } catch {
@@ -48,22 +51,29 @@ const Characters = () => {
 
   return (
     <section className={styles.characters_container}>
-      <h1 className={styles.rick_characters}>Characters</h1>
-      <div className={styles.search_container}>
-        <input onChange={(event) => handlerCharacter(event)} className={styles.search_input} type="text" placeholder="Search by name or species" />
-      </div>
-      {characters.length > 0 && <div className={styles.characters_list}>
-        {characters.map((character: RMC, idx) => {
-          return (
-            <div className={styles.character_item} key={idx} style={{ backgroundImage: `url(${character?.image})` }}>
-              <div className={styles.character_info}>
-                <p>{character.name}</p>
-                <p>{character.species}</p>
+      <div className={styles.characters_details}>
+        <h1 className={styles.rick_characters}>Characters</h1>
+        <div className={styles.search_container}>
+          <input onChange={(event) => handlerCharacter(event)} className={styles.search_input} type="text" placeholder="Search by name or species" />
+        </div>
+        {characters.length > 0 && <div className={styles.characters_list}>
+          {characters.map((character: RMC, idx) => {
+            return (
+              <div className={styles.character_item} key={idx} style={{ backgroundImage: `url(${character?.image})` }}>
+                <div className={styles.character_info}>
+                  <p>{character.name}</p>
+                  <p>{character.species}</p>
+                </div>
               </div>
-            </div>
-          )
-        })}
-      </div>}
+            )
+          })}
+        </div>}
+      </div>
+      <div className={styles.characters_paginator}>
+        <div className={styles.paginator_item}>
+
+        </div>
+      </div>
     </section>
   )
 }
